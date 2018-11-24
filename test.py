f0f5cb901a56e7ce3deb2fad5235fb333c4b21e3
rootv7 <rootv18@gmail.com>
2018-11-24 15:33:38 +0530

    :|

---
+++ test.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+	if event == cv.EVENT_LBUTTONDBLCLK:
+		cv.circle(img,(x,y),100,(255,0,0),-1)
+
+img = cv.imread('1.tif',0)
+cv.namedWindow('original', cv.WINDOW_NORMAL)
+cv.setMouseCallback('original',draw_circle)
+
+while 1:
+	cv.imshow('original',img) 
+	hist = cv.calcHist([img],[0],None,[256],[0,256])
+	plt.plot(hist)
+	plt.show()
+	if cv.waitKey(100) & 0xFF == 27:
+		break 
+
+cv.destroyAllWindows()
+cv.imwrite('1saved.png',img)
